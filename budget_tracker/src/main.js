9d64535073a8327c561121f930e532f1ce52048f
WojtekMurszewski <wojtek.murszewski@gmail.com>
2024-05-22 18:22:18 +0200

    automatyczne przekierowanie na stronę główną jeśli ktoś jest już zalogowany

--- budget_tracker/src/main.js
+++ budget_tracker/src/main.js
@@ -5,20 +5,23 @@ import router from './router';
 import HeaderCash from './components/HeaderCash.vue';
 import FooterMenu from './components/FooterMenu.vue';
 import './registerServiceWorker';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
+// Funkcja obsługująca logowanie i rejestrację
 window.handleSignInSignUp = async (email, password) => {
+    const auth = getAuth();
     try {
         // Sprawdź, czy użytkownik istnieje
-        const userCredential = await signInWithEmailAndPassword(email, password);
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
         console.log('Zalogowano:', userCredential.user);
         router.push('/');
     } catch (error) {
         // Jeśli użytkownik nie istnieje, utwórz nowego
         if (error.code === 'auth/user-not-found') {
             try {
-                const userCredential = await createUserWithEmailAndPassword(email, password);
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
                 console.log('Zarejestrowano:', userCredential.user);
+                router.push('/');
             } catch (error) {
                 console.error('Błąd podczas rejestracji:', error);
             }
@@ -28,11 +31,29 @@ window.handleSignInSignUp = async (email, password) => {
     }
 };
 
-const appVue = createApp(App);
-appVue.use(router); // Zintegruj router z aplikacją Vue
-appVue.component('HeaderCash', HeaderCash);
-appVue.component('FooterMenu', FooterMenu);
+// Inicjalizacja aplikacji Vue
+let app;
+const auth = getAuth();
 
-appVue.config.globalProperties.$db = db; // Dodajemy Firestore do globalnych właściwości
+onAuthStateChanged(auth, (user) => {
+    if (!app) {
+        app = createApp(App);
+        app.use(router); // Zintegruj router z aplikacją Vue
+        app.component('HeaderCash', HeaderCash);
+        app.component('FooterMenu', FooterMenu);
 
-appVue.mount('#app');
+        app.config.globalProperties.$db = db; // Dodajemy Firestore do globalnych właściwości
+
+        app.mount('#app');
+    }
+
+    if (user) {
+        console.log('User is logged in:', user);
+        // Użytkownik jest zalogowany, przekieruj na stronę główną
+        router.push('/main');
+    } else {
+        console.log('No user is logged in.');
+        // Użytkownik nie jest zalogowany, przekieruj na stronę logowania
+        router.push('/');
+    }
+});
